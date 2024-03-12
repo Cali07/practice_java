fadaa84253c8a9b8fa83d439fcf0e42dc0a867d3
mlilo <cmlilo2004@gmail.com>
2024-03-12 15:18:59 +0200

    Added context to what im learning

--- Hello_1.java
+++ Hello_1.java
@@ -2,11 +2,13 @@ class Hello_1{
 
     public static void main(String[] args) {
         StringBuffer var1,var2,var3;
-
-        var1 =  new StringBuffer("CALI");
+        // String buffer has 3 intresting methods delete and insert and replace, reverse , length,indexOf, lastIndexOf
+        var1 =  new StringBuffer("the way to life is Jesus");
         var2 = new StringBuffer(" 2024");
-        var1.delete(0, 1);
-        System.out.println(var1);
+        // var1.delete(0, 1);
+        //This thind deletes it in memoney so it has some good use bro
+        var1.lastIndexOf("life");
+        System.out.println(var1.indexOf("life"));
 
     }
 
