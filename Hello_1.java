d374169018df1f341a3266d80db1cc26c179bb45
mlilo <cmlilo2004@gmail.com>
2024-03-12 14:36:55 +0200

    Practice work for java learning string buffer

---
+++ Hello_1.java
@@ -0,0 +1,14 @@
+class Hello_1{
+
+    public static void main(String[] args) {
+        StringBuffer var1,var2,var3;
+
+        var1 =  new StringBuffer("CALI");
+        var2 = new StringBuffer(" 2024");
+        var1.delete(0, 1);
+        System.out.println(var1);
+
+    }
+
+
+}
