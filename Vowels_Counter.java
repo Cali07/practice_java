b864bfdb89da1b44234fecd71551e06c04003f49
mlilo <cmlilo2004@gmail.com>
2024-03-15 08:24:22 +0200

    Done assignment

--- Vowels_Counter.java
+++ Vowels_Counter.java
@@ -6,19 +6,11 @@ public class Vowels_Counter {
         String vowels = "AEIOU".toLowerCase();
         Integer number_of_vowels = 0;
         for(int i = 0; i < vowels.length(); i++){
-            for(int y = 0; y <var1.toLowerCase().length(); y++ ){
+            for(int y = 0; y <var1.length(); y++ ){
                 if (var1.charAt(y)==(vowels.charAt(i))){
                     number_of_vowels++;
-                }
-
-                
-            }
-            
-
-        }
+                }}}
         System.out.println(number_of_vowels);
-
-
     }
 
 }
