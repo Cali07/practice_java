1f5b7284bd68a465c0e6c56551bc6e54ca394505
cmlilo023 <cmlilo023@student.wethinkcode.co.za>
2024-04-04 11:26:32 +0200

    Demonstarated my uderstaing of pakeges in java

---
+++ names/Balance.java
@@ -0,0 +1,13 @@
+package names;
+
+import java.util.Random;
+
+public class Balance {
+    public void main() {
+        Random p = new Random();
+        System.out.println("You may have "+p.nextInt(100000,1000000000)+ " in your bank account.");
+        
+    }
+
+    
+}
--- testing.java
+++ testing.java
@@ -1,14 +1,16 @@
 import names.My_name_surname;
-
+import names.Balance;
 
 public class testing {
     public static void main(String[] args) {
         String a,b;
         My_name_surname p = new My_name_surname();
+        Balance bal = new Balance();
         String[] big_list = p.forenames();
         a = big_list[0];
         b = big_list[1];
         p.showman(a, b);
+        bal.main();
 
     }
 }
